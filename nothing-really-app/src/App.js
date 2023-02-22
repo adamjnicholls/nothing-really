829d069ec133cb8e943fde3dbdba6572630821ec
Adam <adam.nicholls@gmail.com>
2023-02-22 19:49:22 +0000

    added list of most wanted from FBI list

--- nothing-really-app/src/App.js
+++ nothing-really-app/src/App.js
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
 
 function App() {
+  const [wantedlist, setWantedlist] = useState([]);
+
+  const fetchData = () => {
+    return fetch("https://api.fbi.gov/wanted/v1/list")
+          .then((response) => response.json())
+          .then((data) => setWantedlist(data.items));
+  }
+
+  useEffect(() => {
+    fetchData();
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,9 +28,14 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React now
         </a>
       </header>
+      <ul>
+        {wantedlist && wantedlist.length > 0 && wantedlist.map((listObj, index) => (
+            <li key={listObj.uid}>{listObj.title}</li>
+          ))}
+      </ul>
     </div>
   );
 }
